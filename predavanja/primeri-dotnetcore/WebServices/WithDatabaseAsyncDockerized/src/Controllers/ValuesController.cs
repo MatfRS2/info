eec9b12ca4eefd5e792cbb4bf7a921ccd86ff686
Vladimir Filipovic <vladofilipovic@hotmail.com>
2019-12-25 10:03:25 +0100

    dodao projekat

---
+++ predavanja/primeri-dotnetcore/WebServices/WithDatabaseAsyncDockerized/src/Controllers/ValuesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DIinCore.Controllers
+{
+    [Route("api/[controller]")]
+    /// <summary>
+    /// Kontroler za vrednosti
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        /// <summary>
+        /// Gets this instance.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        /// <summary>
+        /// Gets the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        /// <summary>
+        /// Posts the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        /// <summary>Puts the specified identifier.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="value">The value.</param>
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        /// <summary>
+        /// Brise elemente kobajagi...
+        /// </summary>
+        /// <param name="id">
+        /// Identifikator parametra koji treba obrisati kobajagi 
+        /// </param>
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
