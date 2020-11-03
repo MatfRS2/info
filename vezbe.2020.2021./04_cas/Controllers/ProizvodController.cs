6384b7ad627ae70526ed056fd4ccd1e228e95392
danijelasimic <danijela.p9@gmail.com>
2020-11-03 09:04:41 +0100

    Dodat cetvrti cas

---
+++ vezbe.2020.2021./04_cas/Controllers/ProizvodController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Prodavnica2.Models;
+using Prodavnica2.Models.ViewModels;
+
+namespace Prodavnica2.Controllers
+{
+    public class ProizvodController : Controller
+    {
+        private IProizvodRepozitorijum repozitorijum;
+        public int VelicinaStrane = 4;
+
+        public ProizvodController(IProizvodRepozitorijum repo)
+        {
+            repozitorijum = repo;
+        }
+
+        public ViewResult Spisak(string kategorija, int Strana = 1) => View(
+            new SpisakViewModel
+            {
+                Proizvodi = repozitorijum.Proizvodi
+                        .Where(p => kategorija == null ? true :
+                                                         p.Kategorija == kategorija)
+                        .OrderBy(proizvod => proizvod.ProizvodID)
+                        .Skip((Strana - 1) * VelicinaStrane)
+                        .Take(VelicinaStrane),
+
+                ModelStrane = new PageInfo
+                {
+                    TrenutnaStrana = Strana,
+                    BrojArtikalaPoStrani = VelicinaStrane,
+                    BrojArtikala = kategorija == null ? 
+                                    repozitorijum.Proizvodi.Count() :
+                                    repozitorijum.Proizvodi.Where(p => p.Kategorija == kategorija)
+                                    .Count()
+                                    
+                },
+                TrenutnaKategorija = kategorija
+            }
+            );
+
+
+        //public ViewResult Spisak(int Strana = 1) =>
+        //    View(repozitorijum.Proizvodi
+        //        .OrderBy(proizvod => proizvod.ProizvodID)
+        //        .Skip((Strana - 1) * VelicinaStrane)
+        //        .Take(VelicinaStrane));
+        
+
+        public ViewResult Spisak2()
+        {
+            var proizvodi = repozitorijum.Proizvodi.Where(p => p.Cena > 25).ToList<Proizvod>();
+            ViewBag.BrojProizvoda = proizvodi.Count();
+
+            return View(proizvodi);
+        }
+    }
+}
