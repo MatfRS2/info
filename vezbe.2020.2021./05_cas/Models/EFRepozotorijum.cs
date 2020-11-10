0ff032403152ff0b6bd1583b110fbc0013ca4b28
danijelasimic <danijela.p9@gmail.com>
2020-11-10 09:11:57 +0100

    Dodat peti cas

---
+++ vezbe.2020.2021./05_cas/Models/EFRepozotorijum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prodavnica2.Models
+{
+    public class EFRepozotorijum : IProizvodRepozitorijum
+    {
+        private ApplicationDbContext repozitorijum;
+
+        public EFRepozotorijum(ApplicationDbContext repo)
+        {
+            repozitorijum = repo;
+        }
+
+        public IQueryable<Proizvod> Proizvodi => repozitorijum.Proizvodi;
+    }
+}
---
+++ vezbe.2020.2021./05_cas/Models/IProizvodRepozitorijum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prodavnica2.Models
+{
+    public interface IProizvodRepozitorijum
+    {
+        IQueryable<Proizvod> Proizvodi { get; }
+    }
+}
